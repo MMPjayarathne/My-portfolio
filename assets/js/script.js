b5f99c78b35cf8ea530938acc1ebeeb2ee2efc00
Masith Pramuditha <72557324+MMPjayarathne@users.noreply.github.com>
2023-07-31 19:26:02 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,7 @@
 
   function downloadFile(filename) {
     // Construct the file path for the download
-    const filePath = `../files/${filename}`;
+    const filePath = `../assets/files/${filename}`;
   
     // Create a temporary link element
     const link = document.createElement('a');
@@ -56,7 +56,7 @@
   
   function viewFile(filename) {
     // Construct the file path for the view
-    const filePath = `../files/${filename}`;
+    const filePath = `../assets/files/${filename}`;
   
     // Open the file in a new window or tab
     window.open(filePath, '_blank');
